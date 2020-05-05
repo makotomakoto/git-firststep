08635d44666ac76ad01df4a17e842f30a765b3a3
makoto <makoto.hayase@gmail.com>
2020-05-05 15:50:43 +0900

    add

--- himeclip.js
+++ himeclip.js
@@ -2,12 +2,14 @@
 
 const axios = require('axios');
 const fs = require('fs'); //注: npm i 不要
+const qs = require('querystring');
 
 //データ更新関数
 async function updateData(newData){
     const PATH = './docs/data.json';
     fs.writeFileSync(PATH, JSON.stringify(newData));
 }
+
 
 //検索用の日付キー作成
 function search_date_key(set_date,set_month,set_year){
@@ -17,10 +19,11 @@ function search_date_key(set_date,set_month,set_year){
   return set_date + ' ' + monthShortNames[set_month] + ' ' + set_year
 }
 
-const LINE_NOTIFY_API_URL = 'https://notify-api.line.me/api/notify';
-//const LINE_NOTIFY_TOKEN = 'LINE_NOTIFY_TOKEN';
-const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN;
 
+//LINE
+const LINE_NOTIFY_API_URL = 'https://notify-api.line.me/api/notify';
+//const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN;
+const LINE_NOTIFY_TOKEN = '41kHgMvKf2kDMeOJT98JlkEgxzJhI21zpAli1Jmh2Vh';
 
 let config = {
     url: LINE_NOTIFY_API_URL,
@@ -35,19 +38,18 @@ let config = {
 }
 
 
-//今日の日付取得
-var today = new Date();
-
-set_year = today.getFullYear();
-set_date = today.getDate();
-set_month = today.getMonth();
-
-
-
 // 実際にデータを取得する getRequest 関数
-async function getRequest(set_date,set_month,set_year) {
+async function getRequest(today) {
   
   let response;
+
+
+  let set_year = today.getFullYear();
+  let set_date = today.getDate();
+  let set_month = today.getMonth();
+
+  let set_week_str = [ "日", "月", "火", "水", "木", "金", "土" ][today.getDay()] ;	// 曜日(日本語表記)
+
 
   date_key = search_date_key(set_date,set_month,set_year)
 
@@ -87,17 +89,50 @@ async function getRequest(set_date,set_month,set_year) {
     console.log("image_value::" + image_value)
 
 
-    //unko = unko.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''); //整形2: タグを削除
-    //console.log(result);
+
+    //ファイル作成
     await updateData({ "onthisday_valu": onthisday_value,"description_value":description_value,"link_value":link_value, "image_value":image_value }); //データ更新関数を実行
+
+
+    // メッセージ構成
+    // 文字列を+=で連結していく。
+    // ダブルクオーテーションで囲むと "\n" で改行も加えられる特性がある。
+    let messageText = "\n";
+    messageText += "おはようございます(smile)！\n "
+    messageText +=  (set_month+1) + "月" + set_date + "日" + set_week_str + "曜日"
+    messageText += "の「日めクリップ」です。\n\n"
+    messageText += "きょうの出来事\n「"+ onthisday_value +"」\n\n" ;
+    messageText += description_value + "\n\n" ;
+    messageText +=  link_value;
+
+    // config のメッセージを送る部分 config.data を上書き
+    config.data =  qs.stringify({
+      imageFullsize:image_value,
+      imageThumbnail: image_value,
+      message: messageText
+    });
+
+    ////// LINE Notify に送る ////////////////////////
+    try {
+      const responseLINENotify = await axios.request(config);
+      console.log(responseLINENotify.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+
 
   } catch (error) {
     console.error(error);
   }
 }
 
+
+//今日の日付取得
+var today = new Date();
+
 // getRequest を呼び出してデータを読み込む
-getRequest(set_date,set_month,set_year);
+getRequest(today);
 
 
 
